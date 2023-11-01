5fc415c847ce1770ffcc2c6060154331d0d6ad85
John <johnflanagan827@gmail.com>
2023-11-01 11:40:18 -0400

    make it better

--- index.py
+++ index.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, jsonify, request, make_response
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity
